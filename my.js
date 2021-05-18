9ac2cc1ffbd463ca27b81e3030d403cc6eb94572
duchainguyen611 <83647081+duchainguyen611@users.noreply.github.com>
2021-05-18 19:52:46 +0700

    Update my.js

--- my.js
+++ my.js
@@ -2,6 +2,6 @@ let moneylend = +prompt("Nhap so tien vay:");
 let interest = +prompt("Nhap lai xuat:");
 let year = +prompt("Nhap so nam vay:");
 
-moneyget = moneylend + year*interest;
+moneyget = moneylend + year*(interest/100);
 
 alert(" So tien phai tra sau " + year + " nam la:" +moneyget);
