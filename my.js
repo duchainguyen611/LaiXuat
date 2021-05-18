05542eb31b024ac7c1be5d3ac45755607ba33537
duchainguyen611 <duchaiprovip01@gmail.com>
2021-05-18 19:39:55 +0700

    Laixuat

---
+++ my.js
@@ -0,0 +1,7 @@
+let moneylend = +prompt("Nhap so tien vay:");
+let interest = +prompt("Nhap lai xuat:");
+let year = +prompt("Nhap so nam vay:");
+
+moneyget = moneylend + year*interest;
+
+alert(" So tien phai tra sau " + year + " nam la:" +moneyget);
